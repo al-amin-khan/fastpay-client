3eb3394eaac5dfce2874e61c865d312ed8de0a99
Al Amin Khan <alaminkhan2010@gmail.com>
2025-11-11 18:31:52 +0600

    feat: add update bill button and it's functionality

--- src/pages/MyBills.jsx
+++ src/pages/MyBills.jsx
@@ -1,16 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useAuth from "../hooks/useAuth";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 import Loading from "../components/Loading";
 import { SquarePen, Trash } from "lucide-react";
+import { Link } from "react-router";
+import Swal from "sweetalert2";
 
 const MyBills = () => {
     const axiosPublic = useAxiosPublic();
     const { user } = useAuth();
     const [myBills, setMyBills] = useState([]);
+    const [selectedBill, setSelectedBill] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [refetch, setRefetch] = useState(false);
+
+
+    const billUpdateModalRef = useRef(null);
 
     useEffect(() => {
+        if (!user?.email) return;
         (
             async () => {
                 try {
@@ -28,11 +37,79 @@ const MyBills = () => {
                 }
             }
         )();
-    }, [user.email, setMyBills, axiosPublic]);
+    }, [refetch, user.email, axiosPublic]);
 
     if (loading) return <Loading />;
 
+    if (!user && !user.email) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <p className="text-2xl font-semibold">Please log in to see your bills.</p>
+                <Link to="/auth/login"><button className="btn btn-neutral ml-4">Login</button></Link>
+            </div>
+        )
+    }
+
     const totalBill = myBills.reduce((total, bill) => total + Number(bill.amount), 0);
+
+    const handleUpdateModal = (bill) => {
+        console.log(bill);
+        setSelectedBill(bill);
+        billUpdateModalRef.current.showModal();
+    }
+
+    const handleUpdateBill = async (e) => {
+        e.preventDefault();
+
+        const payload = {
+            billId: e.target.billId.value,
+            accountNumber: e.target.accountNumber.value,
+            amount: e.target.amount.value,
+            billingMonth: e.target.billingMonth.value,
+            username: e.target.username.value,
+            email: user?.email,
+            phone: e.target.phone.value,
+            address: e.target.address.value,
+            updatedAt: new Date(),
+        }
+
+        console.log('updated payload:', payload);
+
+        try {
+            setIsSubmitting(true);
+            const res = await axiosPublic.patch(`/my-bills/${selectedBill._id}`, payload);
+
+            if (res.status !== 200) {
+                throw new Error(res.message);
+            }
+
+            if (res.data.modifiedCount === 1) {
+                setIsSubmitting(false);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Your Bill has been updated.",
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+                billUpdateModalRef.current.close();
+                setSelectedBill(null);
+                setRefetch(!refetch);
+            } else {
+                throw new Error(res?.data?.message || 'Update failed');
+            }
+
+        } catch (error) {
+            console.log(error);
+            Swal.fire({ icon: 'error', title: 'Update failed', text: error.message });
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
+
+    const handleDeleteBill = (bill) => {
+        console.log(bill);
+    }
 
     return (
         <div>
@@ -73,10 +150,17 @@ const MyBills = () => {
                                     <td>{bill.phone}</td>
                                     <td> {(new Date(bill.date)).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })} </td>
                                     <th>
-                                        <button className="btn btn-ghost btn-sm lg:tooltip" data-tip="Edit">
+                                        <button
+                                            onClick={() => handleUpdateModal(bill)}
+                                            disabled={isSubmitting}
+                                            className="btn btn-ghost btn-sm lg:tooltip" data-tip="Edit"
+                                        >
                                             <SquarePen className=" text-white" color="#009966" size={25} />
                                         </button>
-                                        <button className="btn btn-ghost btn-sm lg:tooltip" data-tip="Delete">
+                                        <button
+                                            onClick={() => handleDeleteBill(bill._id)}
+                                            className="btn btn-ghost btn-sm lg:tooltip" data-tip="Delete"
+                                        >
                                             <Trash className="text-white" size={25} color="#FF3333" />
                                         </button>
                                     </th>
@@ -86,7 +170,7 @@ const MyBills = () => {
                     </tbody>
                     <tfoot>
                         <tr>
-                            <th>Total Bill: {myBills.length}</th>
+                            <th>Bill Paid: {myBills.length}</th>
                             <th></th>
                             <th></th>
                             <th>Total: BDT {totalBill}</th>
@@ -97,6 +181,54 @@ const MyBills = () => {
                     </tfoot>
                 </table>
             </div>
+
+
+            {/* update bill modal */}
+            <dialog ref={billUpdateModalRef} className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg text-center text-gray-800">Update Bill</h3>
+                    <div>
+                        <form
+                            onSubmit={handleUpdateBill}
+                        >
+                            <fieldset className="fieldset">
+                                <label className="label">Bill ID</label>
+                                <input type="text" name='billId' className="input w-full text-gray-800" placeholder="Bill ID" value={selectedBill?._id} readOnly disabled />
+
+                                <label className="label">Account Number</label>
+                                <input type="text" name='accountNumber' className="input w-full text-gray-800" placeholder="Account Number" defaultValue={selectedBill?.accountNumber} />
+
+                                <label className="label">Amount</label>
+                                <input type="text" name='amount' className="input w-full text-gray-800" placeholder="Amount" defaultValue={selectedBill?.amount} />
+
+                                <label className="label">Billing Month</label>
+                                <input type="text" name='billingMonth' className="input w-full text-gray-800" placeholder="Billing Month" defaultValue={selectedBill?.billingMonth} />
+
+                                <label className="label">Username</label>
+                                <input type="text" name='username' className="input w-full text-gray-800" placeholder="Account Number" defaultValue={selectedBill?.username} />
+
+                                <label className="label">Phone</label>
+                                <input type="text" name='phone' className="input w-full text-gray-800" placeholder="Phone Number" defaultValue={selectedBill?.phone} />
+
+                                <label className="label">Address</label>
+                                <input type="text" name='address' className="input w-full text-gray-800" placeholder="Address" defaultValue={selectedBill?.address} />
+
+                                {
+                                    isSubmitting ?
+                                        <button type="submit" className="btn loading" disabled>Bill Updating...</button>
+                                        :
+                                        <button className="btn btn-neutral mt-4">Bill Update Now</button>
+                                }
+                            </fieldset>
+                        </form>
+                    </div>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn btn-sm">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
