b35453cef7445324cd0c0d30b80fa69337f461a6
Al Amin Khan <alaminkhan2010@gmail.com>
2025-11-13 10:56:07 +0600

    chore: remove timeout property from axios public

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
     baseURL: 'https://fastpay-server-bay.vercel.app/',
-    timeout: 3000,
 });
 
 const useAxiosPublic = () => axiosPublic;
